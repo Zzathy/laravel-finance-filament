9fb842f67cbc483fd7ef8a07c0306991cd20b699
Zzathy <izzafathony27@gmail.com>
2025-07-14 23:26:57 +0700

    feat(account): finish crud

---
+++ app/Filament/Resources/AccountResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AccountResource\Pages;
+use App\Filament\Resources\AccountResource\RelationManagers;
+use App\Models\Account;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\RawJs;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AccountResource extends Resource
+{
+    protected static ?string $model = Account::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Account Name'),
+                Select::make('type')
+                    ->options([
+                        'Bank Account' => 'Bank Account',
+                        'E-Wallet' => 'E-Wallet',
+                        'Cash' => 'Cash',
+                        'Credit Card' => 'Credit Card',
+                        'Investment' => 'Investment',
+                    ])
+                    ->default('Bank Account')
+                    ->label('Account Type'),
+                Select::make('currency')
+                    ->options([
+                        'IDR' => 'IDR',
+                        'USD' => 'USD',
+                    ])
+                    ->default('IDR')
+                    ->label('Currency'),
+                TextInput::make('initial_balance')
+                    ->numeric()
+                    ->default(0.00)
+                    ->label('Initial Balance')
+                    ->mask(RawJs::make('$money($input)'))
+                    ->stripCharacters(',')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Account Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('type')
+                    ->label('Account Type')
+                    ->sortable(),
+                TextColumn::make('currency')
+                    ->label('Currency')
+                    ->sortable(),
+                TextColumn::make('initial_balance')
+                    ->label('Initial Balance')
+                    ->numeric(
+                        decimalPlaces: 0,
+                        thousandsSeparator: ',',
+                    )
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAccounts::route('/'),
+            'create' => Pages\CreateAccount::route('/create'),
+            'edit' => Pages\EditAccount::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/AccountResource/Pages/CreateAccount.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\AccountResource\Pages;
+
+use App\Filament\Resources\AccountResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
+
+class CreateAccount extends CreateRecord
+{
+    protected static string $resource = AccountResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = Auth::id();
+
+        return $data;
+    }
+}
