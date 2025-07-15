c54909f940f47cc6c684f9746374af7fc47aa57f
Zzathy <izzafathony27@gmail.com>
2025-07-15 19:17:42 +0700

    feat(transaction): finish crud

---
+++ app/Filament/Resources/TransactionResource.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransactionResource\Pages;
+use App\Filament\Resources\TransactionResource\RelationManagers;
+use App\Models\Transaction;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\RawJs;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TransactionResource extends Resource
+{
+    protected static ?string $model = Transaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('account_id')
+                    ->label('Account')
+                    ->relationship('account', 'name')
+                    ->required(),
+                Select::make('destination_account_id')
+                    ->label('Destination Account')
+                    ->relationship('destinationAccount', 'name')
+                    ->nullable(),
+                Select::make('category_id')
+                    ->label('Category')
+                    ->relationship('category', 'name')
+                    ->nullable(),
+                Select::make('type')
+                    ->label('Type')
+                    ->options([
+                        'Income' => 'Income',
+                        'Expense' => 'Expense',
+                        'Transfer' => 'Transfer',
+                    ])
+                    ->required(),
+                TextInput::make('amount')
+                    ->label('Amount')
+                    ->numeric()
+                    ->default(0.00)
+                    ->mask(RawJs::make('$money($input)'))
+                    ->stripCharacters(',')
+                    ->required(),
+                TextInput::make('description')
+                    ->label('Description')
+                    ->nullable(),
+                DatePicker::make('transaction_date')
+                    ->label('Transaction Date')
+                    ->required()
+                    ->default(now()),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('account.name')
+                    ->label('Account')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('destinationAccount.name')
+                    ->label('Destination Account')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('category.name')
+                    ->label('Category')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('type')
+                    ->label('Type')
+                    ->sortable(),
+                TextColumn::make('amount')
+                    ->label('Amount')
+                    ->numeric(
+                        decimalPlaces: 0,
+                        thousandsSeparator: ',',
+                    )
+                    ->sortable(),
+                TextColumn::make('description')
+                    ->label('Description')
+                    ->limit(50)
+                    ->sortable(),
+                TextColumn::make('transaction_date')
+                    ->label('Transaction Date')
+                    ->date()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+            'create' => Pages\CreateTransaction::route('/create'),
+            'edit' => Pages\EditTransaction::route('/{record}/edit'),
+        ];
+    }
+}
